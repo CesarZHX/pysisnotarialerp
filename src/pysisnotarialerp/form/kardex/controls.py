61a5ff25c723ebecefc17f2b6a9604739850e7f5
CesarZHX <112836949+CesarZHX@users.noreply.github.com>
2025-08-30 02:42:13 -0500

    feat: add kardex type and number controls

---
+++ src/pysisnotarialerp/form/kardex/controls.py
@@ -0,0 +1,9 @@
+"""Kardex controls."""
+
+from ...sis_notarial_erp.controls import _PANE
+
+KARDEX_WINDOW = _PANE.WindowControl(searchDepth=1, AutomationId="FrmKardex")
+_TOOL_STRIP = KARDEX_WINDOW.ToolBarControl(searchDepth=1, AutomationId="ToolStrip1")
+TYPE_COMBO_BOX = _TOOL_STRIP.ComboBoxControl(searchDepth=1, Name="")
+TYPE_LIST = TYPE_COMBO_BOX.ListControl(searchDepth=1, ClassName="ComboLBox")
+NUMBER_EDIT = _TOOL_STRIP.EditControl(searchDepth=1, Name="")
--- src/pysisnotarialerp/sis_notarial_erp/controls.py
+++ src/pysisnotarialerp/sis_notarial_erp/controls.py
@@ -6,6 +6,7 @@ WINDOW = WindowControl(searchDepth=1, Name=f"SIS NOTARIAL ERP{' '*143}")
 
 _TOOL_STRIP = WINDOW.ToolBarControl(searchDepth=1, Name="ToolStrip1")
 
-KARDEX_BUTTON = _TOOL_STRIP.ButtonControl(
-    searchDepth=1, Name="KARDEX (F1)", IsEnabled=True
-)
+KARDEX_BUTTON = _TOOL_STRIP.ButtonControl(searchDepth=1, Name="KARDEX (F1)")
+
+_PANE_NAME: str = 'Ingrese el tipo de cambio y presione "ENTER".'
+_PANE = WINDOW.PaneControl(searchDepth=1, Name=_PANE_NAME)
