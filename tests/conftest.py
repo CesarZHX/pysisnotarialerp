0c16bd64752569988d966729f7757b10f9568795
CesarZHX <112836949+CesarZHX@users.noreply.github.com>
2025-09-05 11:10:05 -0500

    fix: env var names

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,9 +17,9 @@ def executable_file() -> Path:
 
 @fixture(scope="session")
 def username() -> str:
-    return environ["SIS_NOTARIAL_ERP_EXECUTABLE_USERNAME"]
+    return environ["SIS_NOTARIAL_ERP_USERNAME"]
 
 
 @fixture(scope="session")
 def password() -> SecretStr:
-    return SecretStr(environ["SIS_NOTARIAL_ERP_EXECUTABLE_USERNAME"])
+    return SecretStr(environ["SIS_NOTARIAL_ERP_PASSWORD"])
