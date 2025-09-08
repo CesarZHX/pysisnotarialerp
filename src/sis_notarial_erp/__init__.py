4e2d28137957fc08b30f8c060f2c841cf00283e5
CesarZHX <112836949+CesarZHX@users.noreply.github.com>
2025-09-07 19:29:03 -0500

    chore: disable uiautomation file logger

--- src/sis_notarial_erp/__init__.py
+++ src/sis_notarial_erp/__init__.py
@@ -1,8 +1,12 @@
+from uiautomation import Logger
+
 from sis_notarial_erp.exceptions import SisNotarialErpException
 from sis_notarial_erp.models.kardex.number import KardexNumber
 from sis_notarial_erp.models.kardex.type import KARDEX_TYPES, KardexType
 from sis_notarial_erp.views.login.exceptions import LoginException
 from sis_notarial_erp.views.login.window import LoginWindow
+
+Logger.SetLogFile("")
 
 __all__ = (
     "SisNotarialErpException",
