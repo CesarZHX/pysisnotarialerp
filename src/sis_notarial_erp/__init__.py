6045eede2fad5326a5f4f0f1317a800aabafc8bd
CesarZHX <112836949+CesarZHX@users.noreply.github.com>
2025-09-16 18:21:35 -0500

    chore: make public record importable directly from module

--- src/sis_notarial_erp/__init__.py
+++ src/sis_notarial_erp/__init__.py
@@ -3,6 +3,7 @@ from uiautomation import Logger
 from sis_notarial_erp.exceptions import SisNotarialErpException
 from sis_notarial_erp.models.kardex.number import KardexNumber
 from sis_notarial_erp.models.kardex.type import KARDEX_TYPES, KardexType
+from sis_notarial_erp.models.public_record.model import PublicRecord
 from sis_notarial_erp.views.login.exceptions import LoginException
 from sis_notarial_erp.views.login.window import LoginWindow
 from sis_notarial_erp.views.main.kardex.controls import PUBLIC_RECORDS_TABLE
@@ -14,6 +15,7 @@ __all__ = (
     "PUBLIC_RECORDS_TABLE",
     "LoginException",
     "KardexNumber",
+    "PublicRecord",
     "KARDEX_TYPES",
     "LoginWindow",
     "KardexType",
